e1bc74582c020379ea01b9e97a795da9031359e8
Alisherro <good071104@gmail.com>
2022-10-11 12:50:53 +0600

    commit

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -4,18 +4,18 @@ import 'package:provider/provider.dart';
 import '../models/models.dart';
 import 'grocery_item_screen.dart';
 import 'grocery_list_screen.dart';
+
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: buildGroceryScreen(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          final manager = Provider.of<GroceryManager>(
-            context,
-            listen: false);
+          final manager = Provider.of<GroceryManager>(context, listen: false);
           Navigator.push(
             context,
             MaterialPageRoute(
@@ -30,19 +30,18 @@ class GroceryScreen extends StatelessWidget {
           );
         },
       ),
-      body: buildGroceryScreen(),
     );
   }
 
   Widget buildGroceryScreen() {
     return Consumer<GroceryManager>(
       builder: (context, manager, child) {
-      if (manager.groceryItems.isNotEmpty) {
-        return GroceryListScreen(manager: manager);
-      } else {
-        return const EmptyGroceryScreen();
-      }
-    },
+        if (manager.groceryItems.isNotEmpty) {
+          return GroceryListScreen(manager: manager);
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
     );
   }
 }
